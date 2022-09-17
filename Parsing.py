2d4d2db696ef6fd339aa1435a25813eac2372918
Dmitry Oparin <jim_hendrix@mail.ru>
2022-09-17 14:20:36 +0500

    Final

--- Parsing.py
+++ Parsing.py
@@ -1,6 +1,7 @@
 import csv
 from time import sleep
 from datetime import date, timedelta
+
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.chrome.service import Service as ChromeService
@@ -8,85 +9,125 @@ from webdriver_manager.chrome import ChromeDriverManager
 from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 
-class Parsing():
-    def __init__(self, login, password, csv = 'chart.csv', driver = webdriver.Chrome(), 
-                 url = "https://charts.spotify.com/charts/view/artist-us-weekly/"):
-        self.login = login
-        self.password = password
-        self.url = url
-        self.csv = csv
-        self.driver = driver
+
+class UrlLisCreator():    
+    def __init__(self, url):
+        self. url = url
         
-    def _find_number_of_weeks(self):
-        '''need for _create_list_of_dates'''
+        self.dates_list=[]
+        self.url_list=[]
+        
+    def _find_number_of_weeks(self):        
         self.start_date= date(2021, 10, 21)
         self.end_date= date.today()
         self.delta= self.end_date-self.start_date
         self.delta_week = self.delta // 7
         
-    def _create_list_of_dates(self):
-        '''need for _create_list_of_urls'''
+    def _create_list_of_dates(self):        
         self._find_number_of_weeks()
-        self.dates=[]
+        
         for i in range(self.delta_week.days):    
             self.day = self.start_date+timedelta(weeks=i) #add week
-            self.day_string= self.day.strftime("%Y-%m-%d") #to string
-            self.dates.append(self.day_string)
+            self.day_string= self.day.strftime("%Y-%m-%d") 
+            self.dates_list.append(self.day_string)
             
-    def _create_list_of_urls(self):
-        '''create list of urls for parsing'''
+    def create_list_of_urls(self):        
         self._create_list_of_dates()
-        self.url_list=[] #create empty arrays to fill with urls
-        for date in self.dates:
+        
+        for date in self.dates_list:
             self.url_string = self.url+date
             self.url_list.append(self.url_string)
-            
-    def _login_to_spotify(self):
-        '''need for parsing'''
-        #driver = webdriver.Chrome()
+        
+    def get_list_of_urls(self):        
+        return self.url_list
+        
+        
+class Autorization():    
+    def __init__(self, login, password):
+        self.login = login
+        self.password = password
+        self.driver = webdriver.Chrome()
+        
+    def log_in(self):        
         self.driver.get("https://accounts.spotify.com/ru/login")
-        self.driver.find_element(By.ID,"login-username").send_keys(self.login)
-        self.driver.find_element(By.ID,"login-password").send_keys(self.password)
-        self.driver.find_element(By.ID,"login-button").click()
+        self.driver.find_element(
+            By.ID,"login-username").send_keys(self.login)
+        self.driver.find_element(
+            By.ID,"login-password").send_keys(self.password)
+        self.driver.find_element(
+            By.ID,"login-button").click()
         sleep(3)
+        
+    def get_driver(self):        
+        return self.driver
 
-    def _main_loop(self):
-        '''main page loop'''
-        self._create_list_of_urls()
-        self._login_to_spotify()
-        self.data_list = [] #create the empty list to fill with lists with data and transfers to csv
-        for u in self.url_list:        
-            self.date = u[56:] #date string to save to data
-            self.driver.get(u)    
-            #loading data, waiting for them to appear
-            self.news_elements = WebDriverWait(self.driver, timeout=50).until(
-                lambda d: d.find_elements(
-                    By.CLASS_NAME, "TableRow__TableRowElement-sc-1kuhzdh-0.bANOpw.styled" \
-                    "__StyledTableRow-sc-135veyd-3.lsudt"))
-            self._extract_data()
+    
+class DataExtractor():
+    def __init__(self, driver, list_of_urls):
+
+        self.driver = driver
+        self.list_of_urls = list_of_urls
+
+        self.data_list = []
+
+    def extract_data_from_all_page(self):        
+        for url in self.list_of_urls:        
+            self.date = url[56:]  #date string to save to data_list
+            self.driver.get(url)
             
-    def _extract_data(self):
+            #loading data, waiting for them to appear
+            search_element = "TableRow__TableRowElement-sc-1kuhzdh-0.bANOpw." \
+                             "styled__StyledTableRow-sc-135veyd-3.lsudt"
+            self.news_elements = WebDriverWait(self.driver, timeout=50).until(
+                lambda d: d.find_elements(By.CLASS_NAME, search_element))
+            
+            self._extract_data_from_one_page()
+           
+    def _extract_data_from_one_page(self):        
         '''extract data from one page'''
-        for e in self.news_elements[:10]: #top 10
-            self.raw_data = e.text.split('\n')
+        
+        for element in self.news_elements[:10]: #top 10
+            self.raw_data = element.text.split('\n')
             self.number = self.raw_data[0]
             self.musicant = self.raw_data[2]
-            self.data = []
-            self.data.append(self.number)
-            self.data.append(self.musicant)
-            self.data.append(self.date)
-            self.data_list.append(self.data)
+            
+            self.data_from_one_page = []
+            self.data_from_one_page.append(self.number)
+            self.data_from_one_page.append(self.musicant)
+            self.data_from_one_page.append(self.date)
+            
+            self.data_list.append(self.data_from_one_page)
 
-    def create_csv(self):        
-        '''create csv based on list'''
-        self._main_loop()
-        with open(self.csv, 'w', encoding="utf-8") as chart:    
+    def get_data_list(self):
+        return self.data_list
+            
+            
+class CsvCreator():
+    def __init__(self, name_csv_file, data_list):
+        self.name_csv_file = name_csv_file
+        self.data_list = data_list
+        
+    def create_csv(self):  
+        with open(self.name_csv_file, 'w', encoding="utf-8") as chart:    
             self.w = csv.writer(chart)
             self.w.writerow(['number', 'musician', 'date'])
             self.w.writerows(self.data_list)
-
+            
 if __name__ == '__main__':
-    #specify your username and password when creating an instance
-    pars = Parsing(login = '', password = '')
-    pars.create_csv()
-  
+    
+    url_list_creator = UrlLisCreator(
+        "https://charts.spotify.com/charts/view/artist-us-weekly/")
+    url_list_creator.create_list_of_urls()    
+    list_of_urls = url_list_creator.get_list_of_urls()
+    
+    autorization = Autorization('jim_hendrix@mail.ru', '231290qQ')
+    autorization.log_in()
+    driver = autorization.get_driver()
+    
+    data_extractor = DataExtractor(driver, list_of_urls)
+    data_extractor.extract_data_from_all_page()
+    data_list = data_extractor.get_data_list()
+    
+    csv_creator = CsvCreator('csv_chart.csv', data_list)
+    csv_creator.create_csv()
+
--- visualisation.py
+++ visualisation.py
@@ -1,80 +1,97 @@
-import pandas as pd
-import plotly.express as px
 import statistics
 
-class Visualisation():
-    '''spotify chart visualization (line and bar)'''
-    def __init__(self, csv_file = 'chart.csv'):
-        self.csv_file = csv_file
+import pandas as pd
+import plotly.express as px
+
+
+class CsvReader():
+    def __init__(self, csv_file_name):
+        self.csv_file_name = csv_file_name
+
+    def open_csv(self):
+        self.csv = pd.read_csv(self.csv_file_name)
+
+    def get_csv(self):
+        return self.csv
+    
+
+class LineVisualisation():    
+    def __init__(self, csv):
+        self.csv = csv
         
-    def _open_csv(self):     
-        self.df = pd.read_csv(self.csv_file)
-        
-    def _create_line_chart(self):
-        self._open_csv()
-        self.fig = px.line(self.df, x="date", y="number",
-                           line_group="musician", color = "musician")
+    def create_chart(self):
+        self.fig = px.line(self.csv, x="date", y="number",
+                           line_group="musician", color="musician")
         self.fig.update_yaxes(autorange="reversed")
         self.fig.update_layout(margin=dict(l=15, r=15, t=15, b=15))
         
+    def show_chart(self):
+        self.fig.show()
+
+    def write_chart(self):
+        self.fig.write_html("line_chart.html")
+        
+
+class BarVisualisation():
+    def __init__(self, csv):
+        self.csv = csv         
+
     def _create_the_set_of_musician(self):
-        '''need for bar charts'''
-        self._open_csv()
         self.musicians = set(list()) 
-        for m in self.df['musician']:
-            self.musicians.add(m)
+        for musician in self.csv['musician']:
+            self.musicians.add(musician)
             
-    def _create_an_empty_dictionary(self):
-        '''dictionary(musician / average chart position) need for bar chart'''        
+    def _create_an_empty_dictionary(self):      
         self.dict_for_bar_charts = {}
         self.dict_for_bar_charts['musician'] = []
         self.dict_for_bar_charts['average chart position'] = []
         
-    def _extract_need_data_from_csv_for_bar(self):
-        '''average chart positions'''
-        self._open_csv()
+    def _extract_data_from_csv(self):
         self._create_the_set_of_musician()
         self._create_an_empty_dictionary()
+        
         for musician in self.musicians:
-            self.list_of_places=[] #empty list of places on the chart    
+            self.list_of_places=[]
+            
             #iterate over the places in the chart for the musician
-            self.df_list = self.df[self.df['musician'] == musician]   
-            for m in self.df_list['number']: #save data from column 'number'
-                self.list_of_places.append(m) 
-                self.chart_average = statistics.mean(self.list_of_places)
-                self.chart_average_round = round(self.chart_average, 1)
+            self.csv_filter_by_musician = self.csv[self.csv['musician'] == musician]
+            
+            #save data from column 'number'
+            for number in self.csv_filter_by_musician['number']: 
+                self.list_of_places.append(number)
+                
+            self.chart_average = statistics.mean(self.list_of_places)
+            self.chart_average_round = round(self.chart_average, 1)
+                
             #add data in dict_for_bar_charts
             self.dict_for_bar_charts['average chart position'].append(
                 self.chart_average_round)
             self.dict_for_bar_charts['musician'].append(musician)
             
-    def _create_bar_chart(self):
-        self._extract_need_data_from_csv_for_bar()
-        self.fig2 = px.bar(self.dict_for_bar_charts,
+    def create_chart(self):
+        self._extract_data_from_csv()
+        self.fig = px.bar(self.dict_for_bar_charts,
                            y='average chart position', x='musician')
-        self.fig2.update_xaxes(categoryorder = 'total ascending') #sort
+        self.fig.update_xaxes(categoryorder = 'total ascending') #sort   
         
-    def show_line_chart(self):
-        self._create_line_chart()
-        self.fig.show()
+    def show_chart(self):
+        self.fig.show()   
         
-    def show_bar_chart(self):
-        self._create_bar_chart()
-        self.fig2.show()
-        
-    def write_line_chart(self):
-        self._create_line_chart()
-        self.fig.write_html("chart.html")
-        
-    def write_bar_chart(self):
-        self._create_bar_chart()
-        self.fig2.write_html("chart_average.html")
+    def write_chart(self):
+        self.fig.write_html("chart_average.html")
 
 if __name__ == '__main__':
-    vis = Visualisation()
-    vis.show_line_chart()
-    vis.show_bar_chart()
-    vis.write_line_chart()
-    vis.write_bar_chart()
 
+    csv_reader = CsvReader('csv_chart.csv')
+    csv_reader.open_csv()
+    csv = csv_reader.get_csv()
     
+    line_visualisation = LineVisualisation(csv)
+    line_visualisation.create_chart()
+    line_visualisation.show_chart()
+    line_visualisation.write_chart()
+
+    bar_visualisation = BarVisualisation(csv)
+    bar_visualisation.create_chart()
+    bar_visualisation.show_chart()
+    bar_visualisation.write_chart()
